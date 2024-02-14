3f314294da5ad83dca3470416cbdc88c965dfe1e
GitHub <noreply@github.com>
2024-02-14 06:03:44 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-sources/chronicle.ts
+++ denops/@ddu-sources/chronicle.ts
@@ -1,7 +1,7 @@
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
 import { Denops } from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
-import { assert, is, isArrayOf } from "https://deno.land/x/unknownutil@v3.16.0/mod.ts";
+import { assert, is, isArrayOf } from "https://deno.land/x/unknownutil@v3.16.1/mod.ts";
 
 const kinds = ["read", "write"];
 
